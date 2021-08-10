b509918a7a7fe363ee814214e4f33ec386794fc8
Scionoftruth <charlieallen132@hotmail.com>
2021-08-10 16:22:30 -0500

    Updated Angular Folder Names

--- WarehouseManagementAngular/warehouse-management/src/app/app.component.spec.ts
+++ WarehouseManagementAngular/warehouse-management/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'waste-management'`, () => {
+  it(`should have as title 'warehouse-management'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('waste-management');
+    expect(app.title).toEqual('warehouse-management');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('waste-management app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('warehouse-management app is running!');
   });
 });

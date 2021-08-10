af6cdcc0986b734857e4f02a9799b1f205dd4fb6
Scionoftruth <charlieallen132@hotmail.com>
2021-08-10 15:27:18 -0500

    Create Customer.java

---
+++ WasteManagement/src/main/java/com/wm/models/Customer.java
@@ -0,0 +1,110 @@
+package com.wm.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.criteria.Order;
+
+@Entity
+@Table(name="customers")
+public class Customer {
+
+	@Id
+	@Column(name="user_id", nullable=false)
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int id;
+	
+	@Column(name="first_name", nullable=false)
+	private String firstname;
+	
+	@Column(name="last_name", nullable=false)
+	private String lastname;
+
+	@Column(name="email", nullable=false, unique=true)
+	private String email;
+	
+	@Column(name="address", nullable=false)
+	private String address;
+	
+	@OneToMany(mappedBy="customer", fetch=FetchType.LAZY)
+	private List<Order> orders = new ArrayList<Order>();
+
+	public Customer() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Customer(int id, String firstname, String lastname, String email, String address, List<Order> orders) {
+		super();
+		this.id = id;
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.email = email;
+		this.address = address;
+		this.orders = orders;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
+	}
+
+	public void setOrders(List<Order> orders) {
+		this.orders = orders;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", firstname=" + firstname + ", lastname=" + lastname + ", email=" + email
+				+ ", address=" + address + ", orders=" + orders + "]";
+	}
+	
+	
+}

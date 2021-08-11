361c2a4c857ba050e30905af18d0f503db76d03a
Scionoftruth <charlieallen132@hotmail.com>
2021-08-11 17:02:11 -0500

    Finished Repo

---
+++ WarehouseManagementTesting/src/main/java/MainDriver.java
@@ -0,0 +1,69 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.wm.enums.ItemType;
+import com.wm.enums.OStatus;
+import com.wm.enums.PStatus;
+import com.wm.models.ItemTypes;
+import com.wm.models.Money;
+import com.wm.models.OrderStatus;
+import com.wm.models.PaymentStatus;
+import com.wm.repository.CustOrderRepo;
+import com.wm.repository.CustomerRepo;
+import com.wm.repository.ItemRepo;
+import com.wm.repository.ItemTypesRepo;
+import com.wm.repository.MoneyRepo;
+import com.wm.repository.OrderRepo;
+import com.wm.repository.OrderStatusRepo;
+import com.wm.repository.PaymentStatusRepo;
+import com.wm.repository.UserRepo;
+
+public class MainDriver {
+
+	public static ApplicationContext appContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+	public static CustomerRepo cRepo = appContext.getBean("customerRepo",CustomerRepo.class);
+	public static CustOrderRepo coRepo = appContext.getBean("custOrderRepo",CustOrderRepo.class);
+	public static ItemRepo iRepo = appContext.getBean("itemRepo",ItemRepo.class);
+	public static ItemTypesRepo itRepo = appContext.getBean("itemTypesRepo",ItemTypesRepo.class);
+	public static MoneyRepo mRepo = appContext.getBean("moneyRepo",MoneyRepo.class);
+	public static OrderRepo oRepo = appContext.getBean("orderRepo",OrderRepo.class);
+	public static OrderStatusRepo osRepo = appContext.getBean("orderStatusRepo",OrderStatusRepo.class);
+	public static PaymentStatusRepo psRepo = appContext.getBean("paymentStatusRepo",PaymentStatusRepo.class);
+	public static UserRepo uRepo = appContext.getBean("userRepo",UserRepo.class);
+	
+	public static void main(String[] args) {
+		Money mr = new Money(0,0,0);
+		mRepo.insert(mr);
+		
+		ItemTypes it1 = new ItemTypes(0,ItemType.HOMEGOODS);
+		itRepo.insert(it1);
+		ItemTypes it2 = new ItemTypes(1,ItemType.CLEANINGSUPPLIES);
+		itRepo.insert(it2);
+		ItemTypes it3 = new ItemTypes(2,ItemType.COOKING);
+		itRepo.insert(it3);
+		ItemTypes it4 = new ItemTypes(3,ItemType.INDUSTRY);
+		itRepo.insert(it4);
+		ItemTypes it5 = new ItemTypes(4,ItemType.SPORTINGEQUIPMENT);
+		itRepo.insert(it5);
+		ItemTypes it6 = new ItemTypes(5,ItemType.OTHER);
+		itRepo.insert(it6);
+		
+		OrderStatus os1 = new OrderStatus(0,OStatus.FUFILLED);
+		osRepo.insert(os1);
+		OrderStatus os2 = new OrderStatus(1,OStatus.UNRESOLVED);
+		osRepo.insert(os2);
+		
+		PaymentStatus ps1 = new PaymentStatus(0,PStatus.PAID);
+		psRepo.insert(ps1);
+		PaymentStatus ps2 = new PaymentStatus(1,PStatus.UNPAID);
+		psRepo.insert(ps2);
+		
+		Item i = new Item()
+		
+		
+		Order o = new Order()
+		
+	}
+	
+	
+}
--- WarehouseManagementTesting/src/main/java/com/wm/repository/CustOrderRepo.java
+++ WarehouseManagementTesting/src/main/java/com/wm/repository/CustOrderRepo.java
@@ -1,5 +1,39 @@
 package com.wm.repository;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+
+import com.wm.models.Customer;
+import com.wm.models.CustOrder;
+@Transactional
+@Repository("custOrderRepo")
 
 public class CustOrderRepo {
 
+	private SessionFactory sesFact;
+	
+	@Autowired
+	public CustOrderRepo(SessionFactory ses) {
+		this.sesFact = ses;
+	}
+	
+	public void insert(CustOrder order) {
+		sesFact.getCurrentSession().save(order);
+	}
+	
+	public void update(CustOrder order) {
+		sesFact.getCurrentSession().update(order);
+	}
+	
+	public CustOrder selectOrderById(int id) {
+		return sesFact.getCurrentSession().get(CustOrder.class, id);
+	}
+	
+	public List<CustOrder> selectAllCustOrders(){
+		List<CustOrder> oList = sesFact.getCurrentSession()
+				.createQuery("from CustOrder", CustOrder.class).list();
+		return oList;
+	}
 }

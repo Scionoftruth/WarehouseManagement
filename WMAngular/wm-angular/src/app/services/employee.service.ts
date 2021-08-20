f73f79f252211604df73ce858125ece236af834f
Scionoftruth <charlieallen132@hotmail.com>
2021-08-20 10:57:46 -0500

    Update employee.service.ts

--- WMAngular/wm-angular/src/app/services/employee.service.ts
+++ WMAngular/wm-angular/src/app/services/employee.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { Observable, throwError, Subject } from 'rxjs';
+import {catchError} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
